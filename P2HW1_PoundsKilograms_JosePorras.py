692df6cc2666c193684c33f08d1af2906829adc6
Hosayy <47569801+Hosayy@users.noreply.github.com>
2019-02-12 11:40:54 -0500

    Add files via upload

---
+++ P2HW1_PoundsKilograms_JosePorras.py
@@ -0,0 +1,14 @@
+#This program converts pounds to kilograms
+#2-12-2019
+#P2HW1 Pounds to Kilograms
+#Jose Porras
+
+#Input the weighted pounds
+pounds = float(input('Enter the weighted pounds: '))
+
+#calculate the pounds to kilogams dividing 2.2046
+kilograms = pounds / 2.2046
+
+#dispay the kilograms
+print('The converted pounds is ',format(kilograms, ',.2f'))
+
